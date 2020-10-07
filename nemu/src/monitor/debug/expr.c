8020720aeaf3a868c50383b7741a5bcd1d4ec019
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-07 22:46:06 +0800

    >  compile NEMU 3019244219 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:46:06 up  4:30,  1 user,  load average: 0.61, 0.40, 0.48 8ed18a4117723c8d6359f14ede27e576339ed9a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -210,10 +210,10 @@ uint32_t eval(int p,int q){
 	else
 	{
 		int op=dominant_operator(p,q);
-		printf("op:%d\t",op);
+		//printf("op:%d\t",op);
 		if(tokens[op].type==POINTER || tokens[op].type==NEGATIVE ||tokens[op].type=='!')
 		{
-			uint32_t val=eval(p+1,p+1);
+			uint32_t val=eval(op+1,op+1);
 			switch(tokens[op].type)
 			{
 				case POINTER:return swaddr_read(val,4);
